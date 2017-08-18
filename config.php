6d60863f6d316afd0ccf2d42e3d70b8fda27a395
Israel <israel@aztell.com.br>
2017-08-18 15:45:57 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('./phpagi/phpagi_asmanager.php');
+
+$manager_host = '127.0.0.1';	        // IP DO SERVIDOR ASTERISK
+$manager_user = 'dongle' ;		// USUARIO MANAGER.CONF
+$manager_pass = 'dongle!@#';		// SENHA MANAGER.CONF
+
+?>
